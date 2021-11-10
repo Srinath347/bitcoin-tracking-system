33dd32c25203a5d6808b6805b5da64578b8f82df
Srinath347 <srinathm.777@gmail.com>
2021-11-09 18:20:54 -0600

    setup commit

---
+++ src/main/java/com/db/bts/BtsApplication.java
@@ -0,0 +1,13 @@
+package com.db.bts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/bts/BtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.bts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
