d0a83bfe36de95a5512f643c10a27163980763d9
Srinath Reddy <srinathm.777@gmail.com>
2021-11-24 12:35:19 -0600

    apis

--- src/main/java/com/db/bts/controller/api/BtsApiController.java
+++ src/main/java/com/db/bts/controller/api/BtsApiController.java
@@ -1,21 +1,30 @@
 package com.db.bts.controller.api;
 
+import com.db.bts.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bts/api")
 public class BtsApiController {
 
-    Logger logger = LoggerFactory.getLogger(BtsApiController.class);
+    Logger logger = LoggerFactory.getLogger(BtsApiController.class); 
 
     @GetMapping()
     public ResponseEntity<Object> healthCheck() {
         return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/user/sign_in")
+    public ResponseEntity<User> userSignIn(@RequestParam("email") String email,
+                                           @RequestParam("password") String password) {
+
+        return null;
     }
 
 }
