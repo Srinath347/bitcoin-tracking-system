dcf87819e36d0012313bde304ea7c89057589d81
Sanjana Thomas <sanjanathomas04@gmail.com>
2021-11-28 18:24:18 -0600

    About view

--- src/main/java/com/db/bts/controller/api/BtsApiController.java
+++ src/main/java/com/db/bts/controller/api/BtsApiController.java
@@ -5,23 +5,16 @@ import com.db.bts.entity.User;
 import com.db.bts.service.impl.AdminServiceImpl;
 import com.db.bts.service.impl.UserServiceImpl;
 import lombok.NonNull;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/bts/api")
@@ -71,6 +64,11 @@ public class BtsApiController {
     public ModelAndView adminRegister(Model model) throws Exception {
     	model.addAttribute("user", new User());
    		return new ModelAndView("register");
+    }
+
+    @GetMapping("/about")
+    public ModelAndView about() throws Exception {
+        return new ModelAndView("about");
     }
 
 }
