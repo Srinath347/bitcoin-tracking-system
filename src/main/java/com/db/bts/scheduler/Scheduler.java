c6c6bd71831cfaf20963bad933d8d54e58b0d86a
Sanjana Thomas <sanjanathomas04@gmail.com>
2021-12-05 11:38:52 -0600

    Scheduler update

--- src/main/java/com/db/bts/scheduler/Scheduler.java
+++ src/main/java/com/db/bts/scheduler/Scheduler.java
@@ -3,6 +3,9 @@ package com.db.bts.scheduler;
 import com.db.bts.model.UserTransactionAmountModel;
 import com.db.bts.service.TransactionService;
 import com.db.bts.service.UserService;
+import com.db.bts.service.impl.AccountServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,20 +21,23 @@ import java.util.List;
 @EnableScheduling
 public class Scheduler {
 
+    Logger logger = LoggerFactory.getLogger(AccountServiceImpl.class);
+
     @Autowired
     TransactionService transactionService;
 
     @Autowired
     UserService userService;
 
-    @Scheduled(cron = "${cron.expression}")
+    @Scheduled(cron = "${cron.minute.expression}")
     public void updateUserStatus() throws Exception {
+
         Date prev = new Date();
         prev.setMonth(prev.getMonth()-1);
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(dateFormat.format(prev));
         Date from = dateFormat.parse(dateFormat.format(prev));
         Date to = dateFormat.parse(dateFormat.format(new Date()));
+        logger.info("Running scheduler from {} to {}", from, to);
         List<Integer> goldUsers = new ArrayList<Integer>();
         List<Integer> silverUsers = new ArrayList<Integer>();
 
@@ -43,6 +49,8 @@ public class Scheduler {
                 silverUsers.add(result.getUserId());
             }
         });
+        logger.info("Gold users--> {}", goldUsers);
+        logger.info("Silver users--> {}", silverUsers);
         if(!goldUsers.isEmpty()) {
             userService.updateMembershipStatus(goldUsers, "Gold");
         }
